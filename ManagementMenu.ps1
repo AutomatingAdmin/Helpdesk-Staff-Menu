21f3e445c5f31b821a3c248588d7bc9060e7530b
AutomatingAdmin <46086631+AutomatingAdmin@users.noreply.github.com>
2018-12-24 09:06:28 -0500

    Added check to see if .ps1 files are present

--- ManagementMenu.ps1
+++ ManagementMenu.ps1
@@ -15,25 +15,27 @@ Function mainMenu {
 	Clear-Host
 	# Main Menu Header
 	Write-Host "------------------------ $title ------------------------"`n -foregroundcolor green -backgroundcolor darkgray
+	# Check if the MainMenu folder contains any scripts
+	If ($mmFiles) {
+		# Build the menu choices based on all .ps1 files found in $mmFiles
+		for ($i=0; $i -le $mmFiles.GetUpperBound(0); $i++){
+			Write-Host `t$($i+1)"."($mmFiles[$i].basename) -foregroundcolor white
+		}
+		# Add the advanced menu option as the last choice
+		Write-Host `t$(($mmFiles | measure).count +1)". Advanced Menu"`n -foregroundcolor cyan
+		# Prompt for a choice 
+		Write-Host "Please enter a number... " -nonewline
 
-	# Build the menu choices based on all .ps1 files found in $mmFiles
-	for ($i=0; $i -le $mmFiles.GetUpperBound(0); $i++){
-		Write-Host `t$($i+1)"."($mmFiles[$i].basename) -foregroundcolor white
-	}
-	# Add the advanced menu option as the last choice
-	Write-Host `t$(($mmFiles | measure).count +1)". Advanced Menu"`n -foregroundcolor cyan
-	# Promt for a choice 
-	Write-Host "Please enter a number... " -nonewline
-
-	$choice = Read-Host
-	while(1..(($mmFiles | measure).count +1) -Notcontains $choice){
-		$choice = Read-Host "Enter a number...Or else"
-	}
-	if (1..(($mmFiles | measure).count) -contains $choice){
-		& ("$scripts\MainMenu\"+$mmFiles[[int]$choice-1].name) ### Run the chosen script
-	}
-	elseif ($choice -eq ($mmFiles | measure).count +1){
-		advancedMenu
+		$choice = Read-Host
+		while(1..(($mmFiles | measure).count +1) -Notcontains $choice){
+			$choice = Read-Host "Enter a number...Or else"
+		}
+		if (1..(($mmFiles | measure).count) -contains $choice){
+			& ("$scripts\MainMenu\"+$mmFiles[[int]$choice-1].name) ### Run the chosen script
+		}
+		elseif ($choice -eq ($mmFiles | measure).count +1){
+			advancedMenu
+		}
 	}
 }
 
@@ -43,26 +45,37 @@ Function advancedMenu {
 	Write-Host "------------------------ $title ------------------------" -foregroundcolor green -backgroundcolor darkgray
 	Write-Host "--------------------------- Advanced  Menu ---------------------------" -foregroundcolor gray -backgroundcolor red
 	
-	# Build the menu choices based on all .ps1 files found in $advFiles
-	for ($i=0; $i -le $advFiles.GetUpperBound(0); $i++){
-		Write-Host `t$($i+1)"."($advFiles[$i].basename) -foregroundcolor white 
-	}
-	# Add the Main Menu option as the last choice
-	Write-Host `t$(($advFiles | measure).count +1)". Main Menu"`n -foregroundcolor cyan
-	# Promt for a choice 
-	Write-Host "Please enter a number... " -nonewline 
+	# Check if the advanced folder contains any scripts
+	If ($advFiles) {
+		# Build the menu choices based on all .ps1 files found in $advFiles
+		for ($i=0; $i -le $advFiles.GetUpperBound(0); $i++){
+			Write-Host `t$($i+1)"."($advFiles[$i].basename) -foregroundcolor white 
+		}
+		# Add the Main Menu option as the last choice
+		Write-Host `t$(($advFiles | measure).count +1)". Main Menu"`n -foregroundcolor cyan
+		# Prompt for a choice 
+		Write-Host "Please enter a number... " -nonewline 
 
-	$choice = Read-Host
-	while(1..(($advFiles | measure).count +1) -Notcontains $choice){
-		$choice = Read-Host "Enter a number...Or else"
+		$choice = Read-Host
+		while(1..(($advFiles | measure).count +1) -Notcontains $choice){
+			$choice = Read-Host "Enter a number...Or else"
+		}
+		if (1..(($advFiles | measure).count) -contains $choice){
+			& ("$scripts\AdvancedMenu\"+$advFiles[[int]$choice-1].name) ### Run the chosen script
+		}
+		elseif ($choice -eq ($advFiles | measure).count +1){
+			mainMenu
+		}
 	}
-	if (1..(($advFiles | measure).count) -contains $choice){
-		& ("$scripts\AdvancedMenu\"+$advFiles[[int]$choice-1].name) ### Run the chosen script
+	Else {
+		Write-Host `n`t"No scripts in the advanced folder"
+		# Prompt for a choice 
+		Write-Host `n"Press Enter to return to the Main Menu or any other key to cancel" -foregroundcolor cyan 
+		If (($Host.UI.RawUI.ReadKey()).VirtualKeyCode -eq 13){
+			mainMenu
+		}
+		
 	}
-	elseif ($choice -eq ($advFiles | measure).count +1){
-		mainMenu
-	}
-
 }
 
 # Call the main menu
